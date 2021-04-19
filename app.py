2e2b2f75fff9373bd57a2fb57eba8c6d6bca9fae
Graph Material <tech@10-55-213-108.vlan860.client.wireless.uic.edu>
2021-04-19 17:09:49 -0500

    made it a flask app

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, redirect, url_for, render_template
+import json
+import requests
+
+app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    if request.url.startswith('http://'):
+        url = request.url.replace('http://', 'https://', 1)
+        code = 301
+        return redirect(url, code=code)
+        
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/walk')
+def walk():
+    
+    return render_template('walk.html', lat=userlat, lng=userlng, clr = color, step=step, text=text, clrc=colorcount)
